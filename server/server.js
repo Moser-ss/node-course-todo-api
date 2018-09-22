bca55465cee876c169b480d790c3b32c64516065
Stephane Moser <moser.sts@gmail.com>
2018-09-22 14:27:38 +0100

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -45,6 +45,7 @@ app.post('/todos', (req, res) => {
     })
 })
 
+// TODOS  ENDPOINTS
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({
@@ -242,6 +243,18 @@ app.post('/users/login', (req, res) => {
 
 })
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send({
+            ok: true,
+            message:'User signup'
+        })
+    }, () => {
+        res.status(400).send({
+            ok:false
+        })
+    })
+})
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 })
