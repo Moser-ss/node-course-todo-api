441bfdff4f22f722184c33e553f0e07d7f396cf4
Stephane Moser <moser.sts@gmail.com>
2019-11-03 18:59:47 +0000

    convert to TS

---
+++ src/db/mongoose.ts
@@ -0,0 +1,19 @@
+import * as mongoose from 'mongoose';
+
+const mongoURL = process.env.MONGODB_URI || '';
+
+mongoose
+    .connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => {
+        console.log(`Connection opened to DB ${mongoURL}`);
+    })
+    .catch(error => {
+        console.error(`Fail to connect to DB ${mongoURL} : Error ${error}`);
+    });
+
+export default { mongoose };
