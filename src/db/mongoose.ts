b93a1b0a48a63be4db44121dde16f74ff82b984d
Stephane Moser <moser.sts@gmail.com>
2019-11-03 19:09:50 +0000

    Fix config

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -3,17 +3,17 @@ import * as mongoose from 'mongoose';
 const mongoURL = process.env.MONGODB_URI || '';
 
 mongoose
-    .connect(mongoURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-    })
-    .then(() => {
-        console.log(`Connection opened to DB ${mongoURL}`);
-    })
-    .catch(error => {
-        console.error(`Fail to connect to DB ${mongoURL} : Error ${error}`);
-    });
+	.connect(mongoURL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
+	.then(() => {
+		console.log(`Connection opened to DB ${mongoURL}`);
+	})
+	.catch(error => {
+		console.error(`Fail to connect to DB ${mongoURL} : Error ${error}`);
+	});
 
 export default { mongoose };
