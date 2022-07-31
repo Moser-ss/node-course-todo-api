4ab519b9ac2965aed1a8e993f1bcc7d0e9b9a6ea
Stephane Moser <moser.sts@gmail.com>
2022-07-31 19:09:35 +0100

    Fix breaking changes of mongoose upgrade

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,12 +3,7 @@ const mongoose = require('mongoose');
 const mongoURL = process.env.MONGODB_URI
 mongoose.Promise = global.Promise
 
-mongoose.connect(mongoURL,{ 
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-}).then(() => {
+mongoose.connect(mongoURL).then(() => {
     console.log(`Connection opened to DB ${mongoURL}`);
 })
 .catch((error) => {
